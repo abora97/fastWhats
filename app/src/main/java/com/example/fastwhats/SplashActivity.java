0d26eb77769d95a41af1d05e1f47b0d726c14143
Abora <https://github.com/abora97>
2019-10-03 20:49:00 +0200

    make splash

---
+++ app/src/main/java/com/example/fastwhats/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.fastwhats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startActivityMethod();
+    }
+
+    void startActivityMethod() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
